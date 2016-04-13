b835c524c5d59dd0b567ca22117dc18f3b8269df
hyang <hyang.cn@outlook.com>
2016-04-13 23:06:37 +0800

    learnyounode 4/13 done

---
+++ nodejs/nodeschool/learnyounode/04-my-first-async-io.js
@@ -0,0 +1,29 @@
+var fs = require("fs"); // import file system module
+var argv = process.argv; // get the arguments
+
+var filePath = argv[2]; // get the file Path
+
+/* solution 1: return buffer */
+fs.readFile(filePath,function countLine(err,data){
+	if(err != null){
+		console.log("Err in fs.readFile!");
+	}else{
+		var tmp = data.toString();
+		var lines = tmp.split("\n");
+		console.log(lines.length-1);
+	}
+});
+
+/* solution 2: return string
+	does not work!!
+ */
+// fs.readFile(filePath,function countLine(err,data){
+// 	if(err != null){
+// 		console.log("Err in fs.readFile!");
+// 	}else{
+// 		console.log("Why?");
+// 		console.log(data);
+// 		var lines = data.split("\n");
+// 		console.log(lines.length-1);
+// 	}
+// },'utf8');
--- nodejs/nodeschool/learnyounode/program.js
+++ nodejs/nodeschool/learnyounode/program.js
@@ -2,12 +2,26 @@ var fs = require("fs"); // import file system module
 var argv = process.argv; // get the arguments
 
 var filePath = argv[2]; // get the file Path
-var buf = fs.readFileSync(filePath); // read content from file
-var tmp = buf.toString(); // turn Buffer to String
-var lines = tmp.split("\n"); // split string by "\n"
 
-//for(var i = 0 ; i < lines.length ; i ++){
-//	console.log(lines[i]);
-//}
+/* solution 1: return buffer */
+// fs.readFile(filePath,function countLine(err,data){
+// 	if(err != null){
+// 		console.log("Err in fs.readFile!");
+// 	}else{
+// 		var tmp = data.toString();
+// 		var lines = tmp.split("\n");
+// 		console.log(lines.length-1);
+// 	}
+// });
 
-console.log(lines.length-1);
+/* solution 2: return string */
+fs.readFile(filePath,function countLine(err,data){
+	if(err != null){
+		console.log("Err in fs.readFile!");
+	}else{
+		console.log("Why?");
+		console.log(data);
+		var lines = data.split("\n");
+		console.log(lines.length-1);
+	}
+},'utf8');
