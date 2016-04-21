6634df13c45b28b9f54dffa6a379f66b1da5053b
hyang <yanghaoasian@outlook.com>
2016-04-21 17:48:23 +0800

    stream-adventure 15/15 done

---
+++ nodejs/nodeschool/stream-adventure/1501-secretz.js
@@ -0,0 +1,19 @@
+var crypto = require('crypto');
+var tar = require('tar');
+var zlib = require('zlib');
+var concat = require('concat-stream');
+
+var parser = tar.Parse();
+parser.on('entry', function(e) {
+    if (e.type !== 'File') return;
+
+    var h = crypto.createHash('md5', { encoding: 'hex' });
+    e.pipe(h).pipe(concat(function(hash) {
+        console.log(hash + ' ' + e.path);
+    }));
+});
+
+process.stdin
+    .pipe(crypto.createDecipher(process.argv[2], process.argv[3]))
+    .pipe(zlib.createGunzip())
+    .pipe(parser);
